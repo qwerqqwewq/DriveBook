cc7e74256c92aec83ec53c8940d1e4d42fbd2878
qwerqqwewq <2725543850@qq.com>
2019-07-03 16:13:41 +0800

    Merge branch 'master' of https://github.com/qwerqqwewq/VideoOnline

--- src/main/java/com/zte/drive/entity/Question.java
+++ src/main/java/com/zte/drive/entity/Question.java
@@ -10,11 +10,39 @@ public class Question {
     private Type type;
     private Subject subject;
 
+    public Question() {
+    }
+
+    @Override
+    public String toString() {
+        return "Question{" +
+                "id=" + id +
+                ", type=" + type +
+                ", subject=" + subject +
+                '}';
+    }
+
     public Integer getId() {
         return id;
     }
 
     public void setId(Integer id) {
         this.id = id;
+    }
+
+    public Type getType() {
+        return type;
+    }
+
+    public void setType(Type type) {
+        this.type = type;
+    }
+
+    public Subject getSubject() {
+        return subject;
+    }
+
+    public void setSubject(Subject subject) {
+        this.subject = subject;
     }
 }
