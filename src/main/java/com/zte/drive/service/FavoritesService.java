41ee07a3aed9e8692681c9992344b1c378ec1f42
qwerqqwewq <2725543850@qq.com>
2019-07-03 17:14:07 +0800

    Merge branch 'master' of https://github.com/qwerqqwewq/DriveBook
    # Conflicts:
    #	src/main/java/com/zte/drive/entity/Question.java

---
+++ src/main/java/com/zte/drive/service/FavoritesService.java
@@ -0,0 +1,36 @@
+package com.zte.drive.service;
+
+import com.zte.drive.entity.Favorites;
+import com.zte.drive.entity.User;
+
+import java.util.List;
+
+/**
+ * Created by ASUS on 2019/7/3.
+ */
+public interface FavoritesService {
+    /**
+     * @param favorites
+     * @return  添加行数
+     */
+    int add(Favorites favorites);
+
+    /**
+     * @param user,id
+     * @return 删除行数
+     */
+    int remove(User user,Integer id);
+
+    /**
+     * @param user
+     * @return 收藏列表
+     */
+    List<Favorites> find(User user);
+
+    /**
+     * @param user
+     * @param num
+     * @return 收藏的前几条数据
+     */
+    List<Favorites> findByNum(User user,Integer num);
+}
---
+++ src/main/java/com/zte/drive/service/MistakeService.java
@@ -0,0 +1,61 @@
+package com.zte.drive.service;
+
+import com.zte.drive.entity.Mistake;
+import com.zte.drive.entity.User;
+
+import java.util.List;
+
+/**
+ * Created by ASUS on 2019/7/3.
+ */
+public interface MistakeService {
+    /**
+     * 增加一条错题记录
+     * @param mistake
+     * @return  增加行数
+     */
+    int add(Mistake mistake);
+
+    /**
+     *根据用户id删除一条错题记录
+     * @param user,id
+     * @return 删除行数
+     */
+    int remove(User user,Integer id);
+
+
+    /**
+     *  用户查询其所有错题记录
+     *@param user
+     * @return 错题列表
+     */
+    List<Mistake> findall(User user);
+
+    /**
+     * 用户按错题id查询错题
+     * @param user,id
+     * @return 错题
+     */
+    Mistake findById(User user,Integer id);
+
+    /**
+     * 按试题内容查询用户错题
+     * @param user,content
+     * @return 错题集
+     */
+    List<Mistake> findByContent(User user,String content);
+
+    /**
+     *用户根据试题类型查询
+     * @param type
+     * @return 错题集
+     */
+    List<Mistake> findByType(User user,String type);
+
+    /**
+     * 用户查询最近错的前num道错题
+     * @param user,num
+     * @return 错题集
+     */
+    List<Mistake> findByTime(User user,Integer num);
+}
