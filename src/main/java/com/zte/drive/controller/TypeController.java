f49bcd79311b2088265a98f390c75a9988a50296
方子贤 <1029659518@qq.com>
2019-07-04 14:14:13 +0800

    12345678

---
+++ src/main/java/com/zte/drive/controller/TypeController.java
@@ -0,0 +1,61 @@
+package com.zte.drive.controller;
+
+import com.zte.drive.entity.Type;
+import com.zte.drive.service.TypeService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
+/**
+ * Author:helloboy
+ * Date:2019-07-03 11:17
+ * Description:<描述>
+ */
+@Controller
+@RequestMapping("/type")
+public class TypeController {
+    @Autowired
+    private TypeService typeService;
+
+    //查询所有类型
+    @RequestMapping("/findAll")
+    public String findAll(Model model){
+        List<Type> list= typeService.findAll();
+        model.addAttribute("list",list);
+        return "findAllType";
+    }
+
+    //根据id查询某一类型
+    @RequestMapping("/findById")
+    public String findById(Model model, Integer id) {
+        Type mm = typeService.findById(id);
+        model.addAttribute("mm", mm);
+        return "findById";
+    }
+
+    //增加一个类型
+    @RequestMapping("/addType")
+    public String addType(Type type) {
+        typeService.add(type);
+        return "redirect:findAll";
+    }
+
+    //根据id删除某个类型
+    @RequestMapping("/removeById")
+    public String removeById(Integer id) {
+        typeService.remove(id);
+        return "redirect:findAll";
+    }
+
+    //修改某个类型
+    @RequestMapping("/modify")
+    public String modify(Type type) {
+        typeService.modify(type);
+        return "redirect:findAll";
+    }
+
+}
+
+
--- src/main/java/com/zte/drive/entity/Type.java
+++ src/main/java/com/zte/drive/entity/Type.java
@@ -1,10 +1,6 @@
 package com.zte.drive.entity;
 
-/**
- * @author 刘煦健
- * @date 2019-07-03 8:21
- * Description:t_type表的实体类
- */
+
 public class Type {
     private Integer id;
     private String type;
